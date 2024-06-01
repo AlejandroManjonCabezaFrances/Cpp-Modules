c8e4ed7f6e5a41937b6811e482434c659b7b5b31
AlejandroManjonCabezaFrances <manjon.alejandro@gmail.com>
2024-06-02 00:21:20 +0200

    module 1, ex 02, pointer and reference is ok

--- Module_01/ex02/srcs/main.cpp
+++ Module_01/ex02/srcs/main.cpp
@@ -29,20 +29,19 @@
 
 int main()
 {
-    std::string stringPTR;
-    // std::string stringREF;
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
 
-    stringPTR = "HI THIS IS BRAIN";
+	std::cout << "The memory address of the string variable: " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
 
-    std::cout << &stringPTR << std::endl;
-    std::cout << stringPTR << std::endl;
-    std::cout << stringPTR[0] << std::endl;
-    
-    std::cout <<  stringPTR.size() << std::endl;
-    
-    return (0);
+	std::cout << "------------------------------------------" << std::endl;
+
+	std::cout << "The value of the string variable: " << str << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+	
+	return (0);
 }
-
-// char *ptr:
-
-// ptr = "HI THIS IS BRAIN";
