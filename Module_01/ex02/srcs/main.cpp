fd76e885e0f73500e341b9b340ed2f335273c9a3
AlejandroManjonCabezaFrances <manjon.alejandro@gmail.com>
2024-06-03 08:30:59 +0200

    beginning ex03 to Module 1 --> Weapon and Human

--- Module_01/ex02/srcs/main.cpp
+++ Module_01/ex02/srcs/main.cpp
@@ -6,26 +6,11 @@
 /*   By: amanjon- <amanjon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 08:16:45 by amanjon-          #+#    #+#             */
-/*   Updated: 2024/05/30 08:44:25 by amanjon-         ###   ########.fr       */
+/*   Updated: 2024/06/03 08:20:45 by amanjon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-// Write a program that contains:
-// • A string variable initialized to "HI THIS IS BRAIN".
-// • stringPTR: A pointer to the string.
-// • stringREF: A reference to the string.
-
-// Your program has to print:
-// • The memory address of the string variable.
-// • The memory address held by stringPTR.
-// • The memory address held by stringREF.
-
-// And then:
-// • The value of the string variable.
-// • The value pointed to by stringPTR.
-// • The value pointed to by stringREF.
 
 int main()
 {
