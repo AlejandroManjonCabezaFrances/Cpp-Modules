0159486a954f778290740abe9b9aec3aa4a18cdf
AlejandroManjonCabezaFrances <manjon.alejandro@gmail.com>
2024-07-08 16:29:18 +0200

    study operator assigned

--- Module_02/ex01/include/Fixed.hpp
+++ Module_02/ex01/include/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: amanjon- <amanjon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 12:43:59 by amanjon-          #+#    #+#             */
-/*   Updated: 2024/07/07 12:45:08 by amanjon-         ###   ########.fr       */
+/*   Updated: 2024/07/08 08:54:39 by amanjon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,28 @@
 #define FIXED_HPP
 
 #include <iostream>
+#include <cmath>
+
+/**
+ * Allowed functions : roundf (from <cmath>)
+ * El ejercicio anterior fue un buen comienzo, pero nuestra clase es bastante inútil. Solo puede representar el valor 0.0. Agrega los siguientes constructores públicos y funciones miembro públicas a tu clase:
+
+Un constructor que toma un entero constante como parámetro.
+Convierte dicho entero al valor correspondiente en punto fijo. El valor de los bits fraccionales se inicializa a 8 como en el ejercicio 00.
+
+Un constructor que toma un número de punto flotante constante como parámetro.
+Convierte dicho número al valor correspondiente en punto fijo. El valor de los bits fraccionales se inicializa a 8 como en el ejercicio 00.
+
+Una función miembro float toFloat(void) const;
+que convierte el valor en punto fijo a un valor de punto flotante.
+
+Una función miembro int toInt(void) const;
+que convierte el valor en punto fijo a un valor entero.
+
+Y añade la siguiente función a los archivos de la clase Fixed:
+
+Una sobrecarga del operador de inserción («) que inserta una representación en punto flotante del número en punto fijo en el objeto de flujo de salida pasado como parámetro.
+*/
 
 class Fixed
 {
@@ -21,12 +43,19 @@ class Fixed
 		static const int	bit = 8;
 		int					fixedPoint;
 	public:
+		// Constructors
 		Fixed();
+		Fixed(const int number);
+		Fixed(const float num);
+		// Copy constructor
 		Fixed(const Fixed& constrCopy);
+		// Assigned operator
 		Fixed& operator=(const Fixed& constrCopy);
 		~Fixed();
-		int		getRawBits(void);
-		void	setRawBits(int const raw);
+/* 		int		getRawBits(void);
+		void	setRawBits(int const raw); */
+		int		toInt(void) const;
+		float	toFloat(void) const;
 };
 
 #endif
