054d6f5178091415dd0c1be30658ddfc830db467
AlejandroManjonCabezaFrances <manjon.alejandro@gmail.com>
2024-12-10 08:57:07 +0100

    making module05 ex03, function makeForm ok

--- Module_05/ex03/include/Intern.hpp
+++ Module_05/ex03/include/Intern.hpp
@@ -1,7 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amanjon- <amanjon-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 08:24:42 by amanjon-          #+#    #+#             */
+/*   Updated: 2024/12/10 08:40:26 by amanjon-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef INTERN_HPP
 #define INTERN_HPP
 
 #include "../include/AForm.hpp"
+#include "../include/ShrubberyCreationForm.hpp"
+#include "../include/RobotomyRequestForm.hpp"
+#include "../include/PresidentialPardonForm.hpp"
 
 #include <iostream>
 #include <iomanip>
@@ -11,17 +26,15 @@
  * const: no se modificarán los valores dentro de la función y permite pasar cadenas literales "robotomy request form"
  * &: no pasa una copia, mayor eficiencia
 */
-class Intern : public AForm
+class Intern
 {
-    private:
-
     public:
         Intern();
         Intern(const Intern& constrCopy);
         Intern& operator=(const Intern& constrCopy);
         ~Intern();
 
-        AForm* makeForm(const std::string& nameForm, const std::string& targetForm);
+        AForm* makeForm(const std::string& nameForm, const std::string& target);
 };
 
 #endif
--- Module_05/ex03/srcs/Intern.cpp
+++ Module_05/ex03/srcs/Intern.cpp
@@ -1,35 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amanjon- <amanjon-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 08:24:26 by amanjon-          #+#    #+#             */
+/*   Updated: 2024/12/10 08:54:31 by amanjon-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/Intern.hpp"
 
 Intern::Intern()
 {
-    std::cout << GREEN << " Default constructor Intern" << RESET << std::endl;
+	std::cout << GREEN << " Default constructor Intern" << RESET << std::endl;
 }
 
 Intern::Intern(const Intern& constrCopy)
 {
-    std::cout << GREEN << " Default constructor copy Intern" << RESET << std::endl;
-    *this = constrCopy;
+	std::cout << GREEN << " Default constructor copy Intern" << RESET << std::endl;
+	*this = constrCopy;
 }
 
 Intern& Intern::operator=(const Intern& constrCopy)
 {
-    return (*this);
+	(void) constrCopy;
+	return (*this);
 }
 
 Intern::~Intern()
 {
-    std::cout << RED <<"Default destructor Intern" << RESET << std::endl;
-}1
+	std::cout << RED <<"Default destructor Intern" << RESET << std::endl;
+}
 
 /**
  * const: no se modificarán los valores dentro de la función y permite pasar cadenas literales "robotomy request form"
  * &: no pasa una copia, mayor eficiencia
 */
-AForm*  Intern::makeForm(const std::string& nameForm, const std::string& targetForm)
+AForm*  Intern::makeForm(const std::string& nameForm, const std::string& target)
 {
-    for (int i = 0; i > 3; i++)
-    {
-        std::cout << "i = " << i << std::endl;
-        break;
-    }
+	std::string literalName[3] = {"Shrubbery Creation Form", "Robotomy Request Form", "Presidential Pardon Form"};
+	AForm* internForm = NULL;
+
+	int i = 0;
+	for (; i < 3; i++)
+	{
+		if (nameForm == literalName[i])
+		break;
+	}
+
+	switch(i)
+	{
+		case 0:
+			internForm = new ShrubberyCreationForm(target);
+			return (internForm);
+		case 1:
+			internForm = new RobotomyRequestForm(target);
+			return (internForm);
+		case 2:
+			internForm = new PresidentialPardonForm(target);
+			return (internForm);
+		default:
+			std::cerr << "Error: Form not found" << std::endl;
+			break;
+	}
+	return (NULL);
 }
