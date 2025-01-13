fcc3d21653b8fdde375a9415862c4db98fe14728
AlejandroManjonCabezaFrances <amanjon@Getafec>
2025-01-13 03:00:18 +0100

    module08 ex 01, two functions allright (long && short), print numbers ok, should to make int main bigger & i need ti make functions to create numbers random (srand ...)

--- Module_08/ex01/srcs/Span.cpp
+++ Module_08/ex01/srcs/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: amanjon <amanjon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 01:25:49 by amanjon           #+#    #+#             */
-/*   Updated: 2025/01/10 01:25:50 by amanjon          ###   ########.fr       */
+/*   Updated: 2025/01/13 02:58:11 by amanjon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,83 @@
 
 Span::Span()
 {
+	std::cout << GREEN << " Default constructor Array" << RESET << std::endl;
 }
 
-Span::Span()
+Span::Span(unsigned int N) : maxSize(N)
 {
+	std::cout << GREEN << " parameterized constructor Array" << RESET << std::endl;
 }
 
 Span::Span(const Span& constrCopy)
-{	
+{
+	*this = constrCopy;
+	std::cout << GREEN << " Default constructor copy Array" << RESET << std::endl;
 }
 
 Span& Span::operator=(const Span& constrCopy)
 {
 	if (this != &constrCopy)
 	{
-
+		*this = constrCopy;
 	}
+	std::cout << GREEN << " Default assignment operator Array" << RESET << std::endl;
 
 	return (*this);
 }
 
 Span::~Span()
 {
+	std::cout << RED << " Default destructor Array" << RESET << std::endl;
+}
+
+void	Span::addNumber(int number)
+{
+	if (numbers.size() >= maxSize)
+		throw (std::overflow_error("vector is full !"));
+	numbers.push_back(number);
+}
+
+int	Span::longestSpan()
+{
+	int minNumber;
+	int maxNumber;
+	
+	if (numbers.size() <= 1)
+		throw (std::logic_error("vector size is less than 2"));
+
+	minNumber = *std::min_element(numbers.begin(), numbers.end());
+	maxNumber = *std::min_element(numbers.begin(), numbers.end());
+
+	return (maxNumber - minNumber);
+}
+
+int	Span::shortestSpan()
+{
+	int shortest;
+	int absoluteNumber;
+	
+	if (numbers.size() <= 1)
+		throw (std::logic_error("vector size is less than 2"));
+
+	shortest = std::numeric_limits<int>::max();
+	for (size_t i = 0; i < numbers.size(); ++i)
+	{
+		for (size_t j = i + 1; j < numbers.size(); ++j)
+		{
+			absoluteNumber = std::abs(numbers[i] - numbers[j]);
+			if (absoluteNumber < shortest)
+				shortest = absoluteNumber;
+		}
+	}
+	
+	return (shortest);
+}
+
+void	Span::printNumbers()
+{
+	size_t	i;
+
+	for (i = 0; i < numbers.size(); ++i)
+		std::cout << "numners = " << numbers[i] << std::endl;
 }
--- Module_08/ex01/srcs/main.cpp
+++ Module_08/ex01/srcs/main.cpp
@@ -6,14 +6,59 @@
 /*   By: amanjon <amanjon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 01:25:46 by amanjon           #+#    #+#             */
-/*   Updated: 2025/01/10 01:25:47 by amanjon          ###   ########.fr       */
+/*   Updated: 2025/01/13 02:18:46 by amanjon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Span.hpp"
 
+/**
+ * Llenar el atributo numbers con 10.000 números aleatorios y 
+*/
 int main()
 {
+	try
+	{
+		
+	
+		// 1.- Meter números Manualmente
+		Span mySpan(10000);
+
+		mySpan.addNumber(5);
+		mySpan.addNumber(3);
+		mySpan.addNumber(6);
+		mySpan.addNumber(1);
+		mySpan.addNumber(12);
+
+		mySpan.printNumbers();
+
+		
+
+		// 2.- Crear función meter números (10.000) aleatoriamente
+		
+	}
+		catch (std::exception& error)
+		{
+			std::cout << PINK << std::endl << "Caught exception " << error.what() << RESET << std::endl;
+		}
+	
+	
 
 	return (0);
 }
+
+/**
+ * Main del Subject
+*/
+/* int main()
+{
+Span sp = Span(5);
+sp.addNumber(6);
+sp.addNumber(3);
+sp.addNumber(17);
+sp.addNumber(9);
+sp.addNumber(11);
+std::cout << sp.shortestSpan() << std::endl;
+std::cout << sp.longestSpan() << std::endl;
+return 0;
+} */
